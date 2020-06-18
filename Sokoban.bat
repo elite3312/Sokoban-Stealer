d02aca91f6e9bb467d896b77e1e01f47b804bb5c
dino <dinolee0625xd@gmail.com>
2020-06-19 01:14:48 +0800

    https://github.com/lemon37564/javaProject.git

--- Sokoban.bat
+++ Sokoban.bat
@@ -1,5 +1,4 @@
-cd Sokoban
-cd code
-javac -d . -encoding utf-8 *.java
-java java2020.finalProject.MenuDemo
+cd C:\Users\User\eclipse-workspace\javaProject\Sokoban\code
+javac -cp C:\Users\User\eclipse-workspace\javaProject\jl1.0.1.jar -d . -encoding utf-8 *.java
+java -cp C:\Users\User\eclipse-workspace\javaProject\jl1.0.1.jar;C:\Users\User\eclipse-workspace\javaProject\Sokoban\code java2020.finalProject.MenuDemo
 pause
