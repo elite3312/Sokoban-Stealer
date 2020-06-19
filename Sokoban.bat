15f792d7cacc0b43f752881a681607291def6088
lemon37564 <rdp65536@gmail.com>
2020-06-19 15:42:17 +0800

    調整

--- Sokoban.bat
+++ Sokoban.bat
@@ -1,4 +1,4 @@
-cd C:\Users\User\eclipse-workspace\javaProject\Sokoban\code
-javac -cp C:\Users\User\eclipse-workspace\javaProject\jl1.0.1.jar -d . -encoding utf-8 *.java
-java -cp C:\Users\User\eclipse-workspace\javaProject\jl1.0.1.jar;C:\Users\User\eclipse-workspace\javaProject\Sokoban\code java2020.finalProject.MenuDemo
+cd Sokoban/code
+javac -cp jl1.0.1.jar -d . -encoding utf-8 *.java
+java -cp jl1.0.1.jar; java2020.finalProject.MenuDemo
 pause
